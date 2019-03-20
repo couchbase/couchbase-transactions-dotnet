f51da972efbcc147cd5fb7fb170998aca87ea4d8
Mike Goldsmith <goldsmith.mike@gmail.com>
2019-03-20 12:05:29 +0000

    add empty ThroughputTester project

---
+++ tests/ThrouputTester/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ThroughputTester
+{
+    public class Program
+    {
+        public static async Task Main()
+        {
+            
+        }
+    }
+}
