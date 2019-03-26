cd08ef9e36a25ca51c9cb23a08a67010220a8c13
Mike Goldsmith <goldsmith.mike@gmail.com>
2019-03-26 10:09:37 +0000

    add TransactionClient implemention, including Polly dependency

---
+++ src/Couchbase.Transactions/TransactionClient.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using Couchbase.Core;
+using Couchbase.Transactions.Config;
+using Polly;
+
+namespace Couchbase.Transactions
+{
+    public class TransactionClient : ITransactionClient
+    {
+        private readonly ICluster _cluster;
+        private readonly DateTime _startedAt;
+        private readonly Policy _policy;
+
+        public TransactionConfig Config { get; }
+        public TimeSpan Duration => DateTime.UtcNow.Subtract(_startedAt);
+
+        public TransactionClient(ICluster cluster, TransactionConfig config)
+        {
+            _cluster = cluster;
+            _startedAt = DateTime.UtcNow;
+            Config = config;
+
+            // setup retry policy using Polly
+            //TODO: handle retryable exceptions differently
+            _policy = Policy.Handle<Exception>().Retry(Config.MaxAttempts);
+        }
+
+        public async Task<ITransactionResult> Run(Func<IAttemptContext, Task> transactionLogic)
+        {
+            var transactionContext = new TransactionContext();
+
+            await _policy.Execute(async () =>
+            {
+                var attemptContext = new AttemptContext(transactionContext.TransactionId, Config);
+                await transactionLogic(attemptContext).ConfigureAwait(false);
+            }).ConfigureAwait(false);
+
+            return new TransactionResult(transactionContext.TransactionId, transactionContext.Attempts, Duration);
+        }
+    }
+}
